1116f61b9c384a7555a3d048fafea9ea1b94b644
P.C. Bender <pcbend@gmail.com>
2014-11-24 14:59:12 -0800

    Forgot to add TFileWriter before committing

---
+++ include/TFileWriter.h
@@ -0,0 +1,27 @@
+#ifndef TFILEWRITER_H
+#define TFILEWRITER_H
+
+#include <TObject.h>
+#include <TFile.h>
+#include <TList.h>
+
+#include <TSharcInput.h>
+#include <TObjectManager.h>
+
+
+class TFileWriter : public TObject {
+
+  public:
+    TFileWriter();
+    virtual ~TFileWriter();
+
+    virtual void Print(Option_t *opt="");
+    virtual void Clear(Option_t *opt="");
+
+    static void WriteList(TFile &file, TList *list, Option_t *opt = "");
+    static void WriteFile(const char *fname, Option_t *opt = "RECREATE");
+
+  ClassDef(TFileWriter,0);
+};
+
+#endif
